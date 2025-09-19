0913c50ff9dbc3686608b200d91b335ec4f73e4a
MP Web Developer <mprwdev@gmail.com>
2025-09-19 18:45:34 +0300

    template init

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-const input = document.querySelector(".file-upload__input");
-const label = document.querySelector(".file-upload__label");
-
-input.addEventListener("change", () => {
-  label.textContent = input.files.length > 0 ? [...input.files].map(file => file.name).join(", ") : "File is not chosen";
-});
+console.log("test check");
