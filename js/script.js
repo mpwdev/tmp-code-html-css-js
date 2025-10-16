6d5529b6f56e007e8e9a7bb57b89ddf6d508b34d
MP Web Developer <mprwdev@gmail.com>
2025-10-16 21:32:54 +0300

    menu dropdown

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-console.log("test check");
+document.addEventListener("DOMContentLoaded", () => {
+  const menuBtns = document.querySelectorAll(".menu__btn");
+  const drops = document.querySelectorAll(".dropdown");
+
+  menuBtns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      let currentBtn = e.currentTarget;
+      let drop = currentBtn.closest(".menu__item").querySelector(".dropdown");
+
+      menuBtns.forEach((btn) => {
+        if (btn !== currentBtn) {
+          btn.classList.remove("menu__btn--active");
+        }
+      });
+
+      drops.forEach((item) => {
+        if (item !== drop) {
+          item.classList.remove("dropdown--active");
+        }
+      });
+
+      drop.classList.toggle("dropdown--active");
+      currentBtn.classList.toggle("menu__btn--active");
+    });
+  });
+
+  document.addEventListener("click", (e) => {
+    if (!e.target.closest(".menu")) {
+      menuBtns.forEach((btn) => {
+        btn.classList.remove("menu__btn--active");
+      });
+
+      drops.forEach((item) => {
+        item.classList.remove("dropdown--active");
+      });
+    }
+  });
+});
